6629df7746c2ff7b6c76cd069fc6accb7bf2c8a8
isaacj96 <isaac.jaramillo@g.austincc.edu>
2020-04-17 03:28:23 -0500

    Completed a new excersice

---
+++ chapter_three/changing_guest_list.py
@@ -0,0 +1,16 @@
+#starting with list we created in guest_list.py and change the set of invitations because one guest can't make it
+guest_list = ['Lionel Messi', 'Einstein', 'Elon Musk']
+print(guest_list)
+
+print(f"Hi, {guest_list[0].title()}, would you like to come to dinner with me?")
+print(f"Hi, {guest_list[1].title()}, would you like to come to dinner with me?")
+print(f"Hi, {guest_list[2].title()}, would you like to come to dinner with me?")
+
+#Changes to original program begins here
+
+print(f"Oh no! Looks like {guest_list[0]} won't be able to make it, better look for someone else to invite.")
+
+guest_list[0] = 'Carl Sagan'
+print(f"Hi, {guest_list[0].title()}, would you like to come to dinner with me?")
+print(f"Hi, {guest_list[1].title()}, I had to send out another set of invitations, but would you still like to come to dinner with me?")
+print(f"Hi, {guest_list[2].title()}, I had to send out another set of invitations, but would you still like to come to dinner with me?")
--- chapter_three/motorcycles.py
+++ chapter_three/motorcycles.py
@@ -1,9 +1,11 @@
+#creating list of motorcycles
 motorcycles = ['Honda', 'yamah', 'suzuki']
 print(motorcycles)
 
 motorcycles.append('ducati')
 print(motorcycles)
 
+#starting from empty list and then adding each motorcycle to list
 motorcycles = []
 motorcycles.append('honda')
 motorcycles.append('yamaha')
@@ -11,6 +13,7 @@ motorcycles.append('suzuki')
 
 print(motorcycles)
 
+#inserting ducati as first mortcycle in list
 motorcycles = ['Honda', 'yamah', 'suzuki']
 
 motorcycles.insert(0, 'ducati')
