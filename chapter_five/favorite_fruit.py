64de778783dd1ef8cb36c0bfbb99e14ed6e1b7f0
isaacj96 <isaac.jaramillo@g.austincc.edu>
2020-12-21 02:49:13 -0600

    End Ch.5, Start Ch.6

---
+++ chapter_five/favorite_fruit.py
@@ -0,0 +1,17 @@
+# Make a list of your favorite fruits and then write a series of independent if statements that check for certain
+# fruits in your list
+
+favorite_fruit = ['banana', 'grapes', 'apples', 'pears', 'strawberries']
+
+if 'banana' in favorite_fruit:
+    print("You must like bananas")
+elif 'grapes' in favorite_fruit:
+    print("Grapes yay")
+elif 'tomato' not in favorite_fruit:
+    print("That's not a fruit!")
+elif 'orange' not in favorite_fruit:
+    "Orange not in the list :("
+elif 'strawberries':
+    "You really like strawberries!"
+
+
